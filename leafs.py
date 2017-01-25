e2c8c1d0aca6f1caf801743aae5dbe95aa01d880
Thaddäus Töppen <t.toeppen@gmx.de>
2017-01-25 12:08:31 +0100

    Add labelling

--- leafs.py
+++ leafs.py
@@ -1,39 +1,46 @@
 
 # coding: utf-8
 
-# In[ ]:
 
+get_ipython().magic('matplotlib inline')
 import skimage.io as io
 from skimage.color import rgb2gray
 from glob import glob
 import numpy as np
-
-
-get_ipython().magic('matplotlib inline')
-
-
-# In[ ]:
+from skimage.filter import threshold_otsu
+from skimage import img_as_uint
+from skimage.morphology import closing
+from skimage.morphology import square
+from skimage.measure import label, regionprops
+from skimage.morphology import remove_small_objects
+from skimage.color import label2rgb
 
 files = glob("./leafsnap-dataset/dataset/images/lab/zelkova_serrata/*.jpg")
 
-for f in files:
+for f in files[:10]:
+    # reading data
     im = io.imread(f)
-    # io.imsave(f[:-4] + ".bmp", im)
+    
+    # cropping out the interesting part
+    leaf_grey = rgb2gray(im[:600,:600])
+    
+    # segmenting
+    thresh = threshold_otsu(leaf_grey)
+    
+    # filling holes
+    bw_closed = binary_closing(np.invert(leaf_grey > thresh))
+    
+    # removing small objects outside of leaf
+    bw_closed_rem = remove_small_objects(bw_closed, min_size=128, connectivity=2)
 
-
-# In[ ]:
-
-im1 = io.imread(files[0])
-io.imshow(im1)
-
-
-# In[ ]:
-
-im1_gray = rgb2gray(im1)
-io.imshow(im1_gray)
-
-
-# In[ ]:
-
-
+    # labelling
+    labelled = label(bw_closed_rem)
+    image_label_overlay = label2rgb(labelled, image=bw_closed_rem)
+    
+    properties = regionprops(labelled)
+    
+    
+    # saving
+    io.imsave(f[:-4] + ".bmp", img_as_uint(bw_closed_rem))
+    io.imsave(f[:-4] + "_label.bmp", image_label_overlay)
 
