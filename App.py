2eb1af0ee51fe8ccf1cab4b7dedd1ab19d98ccbe
Thaddäus Töppen <t.toeppen@gmx.de>
2017-03-01 19:35:08 +0100

    Inline all frames into BaseApp

--- App.py
+++ App.py
@@ -5,51 +5,62 @@ import tkinter.ttk as ttk
 from tkinter.filedialog import askopenfilename
 from PIL import ImageTk, Image
 
-from frames.MenuFrame import MenuFrame
-from frames.TabFrame  import TabFrame
-from frames.InfoFrame import InfoFrame
-
-
 # BASE CLASS
 
-class BaseApp(object):
+class BaseApp():
     """missing docstring"""
     def __init__(self):
         # BASIC
-        self.root = tk.Tk()
-        self.root.title("Leaves UI")
-        self.root.geometry("1000x700")
-        self.frame = ttk.Frame(self.root, width=1000, height=700)
-        self.frame.pack(side="top", fill="both", expand=True)
+        root = tk.Tk()
+        root.title("Leaves UI")
+        root.geometry("1000x700")
+        frame = ttk.Frame(root, width=1000, height=700)
+        frame.pack(side="top", fill="both", expand=True)
 
         # MENUBAR
-        menubar = tk.Menu(self.root)
-        self.menu = dict()
-        self.menubar = menubar
+        menubar = tk.Menu(root)
+        menu = dict()
+        menubar = menubar
         menu_file = tk.Menu(menubar)
         menubar.add_cascade(menu=menu_file, label="Datei", underline=0)
         menu_file.add_command(label="Oeffnen...", command=open_file, underline=0)
         menu_file.add_command(label="Beenden", command=quit, underline=1)
-        self.root.config(menu=menubar)
-        self.menu["menubar"] = menubar
-        self.menu["Datei"] = menu_file
+        root.config(menu=menubar)
+        menu["menubar"] = menubar
+        menu["Datei"] = menu_file
 
-        # FRAMES
-        self.menu_frame = MenuFrame(self.frame).getFrame()
-        self.tab_frame = TabFrame(self.frame).get_frame()
-        self.info_frame = InfoFrame(self.tab_frame).getFrame()
+        # MAIN FRAMES
+        menu_frame = ttk.Frame(frame, relief="ridge", borderwidth=2)
+        tab_frame = ttk.Frame(frame, relief="ridge", borderwidth=2)
+        info_frame = ttk.Frame(tab_frame, relief="ridge", borderwidth=2)
 
-        self.menu_frame.pack(side="left", fill="both", expand=False, padx=5, pady=5)
-        self.tab_frame.pack(side="right", fill="both", expand=True, padx=5, pady=5)
-        self.info_frame.pack(side="bottom", fill="x", expand=False, padx=5, pady=5)
+        # PACKING THE MAIN FRAMES
+        menu_frame.pack(side="left", fill="both", expand=False, padx=5, pady=5)
+        tab_frame.pack(side="right", fill="both", expand=True, padx=5, pady=5)
+        info_frame.pack(side="bottom", fill="x", expand=False, padx=5, pady=5)
+
+        # ADDING FRAME LABELS
+        ttk.Label(info_frame, text="info frame").pack()
+        ttk.Label(menu_frame, text="menu frame").pack()
+        ttk.Label(tab_frame, text="tab frame").pack()
+
+        # TAB FRAME CONTENT
+        image_tabs = ttk.Notebook(tab_frame)
+        original_tab = tk.Frame(image_tabs)
+        segmented_tab = tk.Frame(image_tabs)
+        labelled_tab = tk.Frame(image_tabs)
+        image_tabs.add(original_tab, text="Original")
+        image_tabs.add(segmented_tab, text="Segmented")
+        image_tabs.add(labelled_tab, text="Labelled")
+        image_tabs.pack()
 
         # RUN IF INSTANCE CREATED
-        self.root.mainloop()
+        root.mainloop()
 
-    # put the loaded image into a tab
-    # Label(self.tabFrame.imageTabs.originalTab, image=file_image).pack()
+        # put the loaded image into a tab
+        # Label(self.tabFrame.imageTabs.originalTab, image=file_image).pack()
 
-# FUNCTIONS
+# HELPER FUNCTIONS
 
 def get_tk_image(filename):
     """missing docstring"""
