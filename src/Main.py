5816660a12a4ff15c808200b47111852862e0b15
Thaddäus Töppen <t.toeppen@gmx.de>
2017-05-20 16:17:21 +0200

    Add docstrings

--- src/Main.py
+++ src/Main.py
@@ -9,7 +9,7 @@ matplotlib.use("TkAgg")
 import tkinter              as tk
 import tkinter.ttk          as ttk
 import tkinter.scrolledtext as tkst
-import tkinter.filedialog   as tkfd # import askdirectory, asksaveasfilename
+import tkinter.filedialog   as tkfd
 import glob                 as glob
 import skimage.io           as io
 import numpy                as np
@@ -17,7 +17,10 @@ from   PIL                  import ImageTk, Image
 from   util.img_processing  import processing_pipe
 
 class BaseApp():
-    """missing docstring"""
+    """ The entry point for the program.
+        Defines the components of the UI.
+        Call as 'BaseApp() to run. """
+
     def __init__(self):
         # BASIC
         self.root = tk.Tk()
@@ -26,14 +29,14 @@ class BaseApp():
         self.root.attributes("-fullscreen", True)
 
         self.directory_filenames = ["Load an image directory..."]
-        self.selected_image = tk.StringVar()
+        self.selected_image      = tk.StringVar()
 
         # MAIN FRAMES
-        self.menu_frame = ttk.Frame(self.root, relief="ridge", borderwidth=1)
-        self.tab_frame  = ttk.Frame(self.root, relief="ridge", borderwidth=1)
+        self.menu_frame   = ttk.Frame(self.root, relief="ridge", borderwidth=1)
+        self.tab_frame    = ttk.Frame(self.root, relief="ridge", borderwidth=1)
         self.button_frame = ttk.Frame(self.menu_frame, relief="ridge", borderwidth=1)
-        self.drop_frame = ttk.Frame(self.menu_frame, relief="ridge", borderwidth=1)
-        self.info_frame = ttk.Frame(self.menu_frame, relief="ridge", borderwidth=1)
+        self.drop_frame   = ttk.Frame(self.menu_frame, relief="ridge", borderwidth=1)
+        self.info_frame   = ttk.Frame(self.menu_frame, relief="ridge", borderwidth=1)
 
         # MAIN FRAMES - PACKING
         self.menu_frame.pack(side="left", fill="both", expand=False, padx=2, pady=2)
@@ -53,16 +56,16 @@ class BaseApp():
         self.drop = tk.OptionMenu(self.drop_frame,
                                   self.selected_image,
                                   *self.directory_filenames,
-                                  command=self.process_selected)
+                                  command=self.display_images_and_data)
         self.drop["width"] = 30
         self.drop.pack()
 
         # TAB FRAME CONTENT
-        self.image_tabs = ttk.Notebook(self.tab_frame)
-        self.original_tab = tk.Frame(self.image_tabs)
+        self.image_tabs    = ttk.Notebook(self.tab_frame)
+        self.original_tab  = tk.Frame(self.image_tabs)
         self.greyscale_tab = tk.Frame(self.image_tabs)
         self.segmented_tab = tk.Frame(self.image_tabs)
-        self.labelled_tab = tk.Frame(self.image_tabs)
+        self.labelled_tab  = tk.Frame(self.image_tabs)
         self.image_tabs.add(self.original_tab, text="Original")
         self.image_tabs.add(self.greyscale_tab, text="Greyscale")
         self.image_tabs.add(self.segmented_tab, text="Segmented")
@@ -80,7 +83,7 @@ class BaseApp():
 
         # IMAGE LABELS - filled when loading an image
         self.original_image  = ttk.Label(self.original_tab)
-        self.greyscale_image = ttk.Label(self.original_tab)
+        self.greyscale_image = ttk.Label(self.greyscale_tab)
         self.segmented_image = ttk.Label(self.segmented_tab)
         self.labelled_image  = ttk.Label(self.labelled_tab)
 
@@ -90,30 +93,34 @@ class BaseApp():
     # HELPER FUNCTIONS
 
     def select_previous(self):
-        """missing docstring"""
+        """ Select the previous entry in the drop-down menu and redraw images. """
 
         selected_index = self.directory_filenames.index(self.selected_image.get())
 
-        if (selected_index > 0):
+        # theres only a previous image if the first one isn't currently selected
+        if selected_index > 0:
             new_selected_image = self.directory_filenames[selected_index - 1]
             self.selected_image.set(new_selected_image)
-            self.process_selected(self.selected_image.get())
+            self.display_images_and_data(self.selected_image.get())
 
 
     def select_next(self):
-        """missing docstring"""
+        """ Select the next entry in the drop-down menu and redraw images. """
 
         num_files = len(self.directory_filenames)
         selected_index = self.directory_filenames.index(self.selected_image.get())
 
-        if (num_files - 1 > selected_index):
+        if (num_files - 1) > selected_index:
             new_selected_image = self.directory_filenames[selected_index + 1]
             self.selected_image.set(new_selected_image)
-            self.process_selected(self.selected_image.get())
+            self.display_images_and_data(self.selected_image.get())
 
 
     def get_dir_filenames(self):
-        """missing docstring"""
+        """ Takes the BaseApp object.
+            Takes all filenames from an user-specified directory.
+            Displays the filenames in a drop-down menu,
+            selects the first entry and calls the processing function on it. """
 
         images_dir = tkfd.askdirectory(title="Choose image directory", mustexist=True)
         image_names = glob.glob(images_dir+"/*.*")
@@ -126,31 +133,31 @@ class BaseApp():
         self.drop = tk.OptionMenu(self.drop_frame,
                                   self.selected_image,
                                   *self.directory_filenames,
-                                  command=self.process_selected)
+                                  command=self.display_images_and_data)
         self.drop["width"] = 30
         self.drop.pack()
 
-        self.process_selected(self.selected_image.get())
+        self.display_images_and_data(self.selected_image.get())
 
 
-    # display_images_and_data :: ImageFilePath -> IO ()
     def display_images_and_data(self, file_path):
-        """missing docstring"""
+        """ Takes the BaseApp object and a file path.
+            The specified image file is processed, the image object
+            is scaled and displayed together with labelled region data. """
 
-        processed_image_object = processing_pipe(file_path)
+        processed_image_object   = processing_pipe(file_path)
 
-        self.pil_original_image     = Image.fromarray(processed_image_object["original_img"], "RGB")
-        self.pil_greyscale_image    = Image.fromarray(processed_image_object["greyscale_img"], "L")
-        self.pil_segmented_image    = Image.fromarray(processed_image_object["segmented_ubyte_img_bw"])
-        self.pil_labelled_image     = Image.fromarray(processed_image_object["labelled_ubyte_img_rbg"], "RGB")
+        self.pil_original_image  = Image.fromarray(processed_image_object["original_img"], "RGB")
+        self.pil_greyscale_image = Image.fromarray(processed_image_object["greyscale_img"], "L")
+        self.pil_segmented_image = Image.fromarray(processed_image_object["segmented_ubyte_img_bw"])
+        self.pil_labelled_image  = Image.fromarray(processed_image_object["labelled_ubyte_img_rbg"], "RGB")
 
-        regions_properties     = processed_image_object["regions_properties"] 
+        regions_properties       = processed_image_object["regions_properties"] 
 
         # scaling the images
-        frame_width  = int(self.tab_frame.winfo_width() * 0.9)
-        frame_height = int(self.tab_frame.winfo_height() * 0.9)
-        max_size     = (frame_width, frame_height)
+        max_size = (int(self.tab_frame.winfo_width() * 0.9), int(self.tab_frame.winfo_height() * 0.9))
 
+        # Resize images in-place (mutating)
         self.pil_original_image.thumbnail(max_size)
         self.pil_greyscale_image.thumbnail(max_size)
         self.pil_segmented_image.thumbnail(max_size)
@@ -184,12 +191,9 @@ class BaseApp():
         self.region_data.replace(1.0, tk.END, regions_properties)
 
 
-    def process_selected(self, selection):
-        """missing docstring"""
-
-        self.display_images_and_data(selection)
-
     def save_current_tab_image(self):
+        """ Get the currently displayed tab and save it's image. """
+
         current_tab = self.image_tabs.tab(self.image_tabs.select(), "text")
         save_file_name = tkfd.asksaveasfilename(defaultextension="bmp", title="Save currently displayed image")
 
